89b2e44fbdabf1076b818ba3ecc567e3dd80d794
Gautami L. <gautamis98@gmail.com>
2026-01-30 20:32:05 -0500

    upload model

--- task_2/main.tf
+++ task_2/main.tf
@@ -99,10 +99,11 @@ resource "aws_sagemaker_model" "model" {
   name               = "example-model"
   execution_role_arn = aws_iam_role.sagemaker_role.arn
 
-  primary_container {
-  image          = "763104351884.dkr.ecr.us-east-2.amazonaws.com/pytorch-inference:2.1.0-cpu-py310-ubuntu20.04"
+primary_container {
+  image          = "683313688378.dkr.ecr.us-east-2.amazonaws.com/sagemaker-scikit-learn:1.2-1-cpu-py3"
   model_data_url = "s3://${aws_s3_bucket.artifact_bucket.bucket}/${aws_s3_object.model_artifact.key}"
-  }
+}
+
 
 
   depends_on = [aws_s3_object.model_artifact]
